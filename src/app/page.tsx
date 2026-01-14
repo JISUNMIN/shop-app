b77f506da2c8b8dfb0f1d24e9ca8d1c892b4092e
JISUNMIN <gktcld@naver.com>
2026-01-14 14:06:46 +0900

    chore: update env config, add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 import ProductList from "@/app/product/ProductList";
 
 export default function Page() {
-  return
-  <Suspense fallback={<div className="container py-8">Loading...</div>}>  
-    <ProductList />;
-  </Suspense>
+  return (
+    <Suspense fallback={<div className="container py-8">Loading...</div>}>
+      <ProductList />;
+    </Suspense>
+  );
 }
